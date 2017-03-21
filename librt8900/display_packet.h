9002542a4f1aee49752367e888548da787fe4dd2
cob16 <cob16@aber.ac.uk>
2017-03-21 04:24:13 +0000

    get busy and main states

--- librt8900/display_packet.h
+++ librt8900/display_packet.h
@@ -12,7 +12,27 @@
 #include "packet.h"
 
 struct display_packet {
-    PACKET_BYTE arr[42];
+        PACKET_BYTE arr[42];
+};
+
+enum power_level {
+        low = 1,
+        medium = 2,
+        medium2 = 3,
+        high = 4
+};
+
+struct radio_state_sides {
+        int busy;
+        char frequency;
+        enum power_level power_level;
+};
+
+
+struct radio_state {
+        struct radio_state_sides *main; //the currently selected pointer
+        struct radio_state_sides left;
+        struct radio_state_sides right;
 };
 
 #endif //RT8900_SERIAL_CONTROL_DISPLAY_PACKET_H
--- test/test_librt8900/test_display_packet.h
+++ test/test_librt8900/test_display_packet.h
@@ -5,6 +5,16 @@
 #ifndef RT8900_SERIAL_CONTROL_TEST_DISPLAY_PACKET_H
 #define RT8900_SERIAL_CONTROL_TEST_DISPLAY_PACKET_H
 
-class TestDisplayPacket : public ::testing::Test{};
+#include <display_packet.h>
+#include <packet.h>
+
+class TestDisplayPacket : public ::testing::Test {};
+
+class TestDisplayPacketReaders : public ::testing::Test {
+public:
+    virtual void SetUp();
+
+    struct display_packet packet;
+};
 
 #endif //RT8900_SERIAL_CONTROL_TEST_DISPLAY_PACKET_H
